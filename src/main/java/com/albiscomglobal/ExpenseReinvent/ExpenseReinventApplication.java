37217f7c3200bdb508457af58567d9329cc58bae
unknown <ahmedwoye@gmail.com>
2023-10-10 08:54:14 +0100

    first commit

---
+++ src/main/java/com/albiscomglobal/ExpenseReinvent/ExpenseReinventApplication.java
@@ -0,0 +1,13 @@
+package com.albiscomglobal.ExpenseReinvent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpenseReinventApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpenseReinventApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/albiscomglobal/ExpenseReinvent/ExpenseReinventApplicationTests.java
@@ -0,0 +1,13 @@
+package com.albiscomglobal.ExpenseReinvent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpenseReinventApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
